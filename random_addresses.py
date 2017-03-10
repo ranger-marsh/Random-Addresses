c0f8c46d8c6494cb5569e3de05b8e4d64ee4371d
Scott <scott48074@users.noreply.github.com>
2017-03-10 14:36:06 -0600

    Getting some work done on the address randomizer

--- random_addresses.py
+++ random_addresses.py
@@ -1,5 +1,46 @@
+import random
+
+
+def open_file(path):
+    with open(path, 'r') as file:
+        addresses = file.read().splitlines()
+        addresses = map(str.strip, addresses)  # Remove white space
+        addresses = list(filter(None, addresses))  # Remove empty lines
+    return addresses
+
+
+def write_file(random_list, path):
+    with open(path, 'w') as file:
+        for address in random_list:
+            file.write('{}\n'.format(address))  # write list line by line
+
+
+def determine_sample_size(address_list, sample_rate):
+    sample_size = int(len(address_list) * sample_rate)
+    return sample_size
+
+
+def random_sample(address_list, sample_size):
+    random_list = random.sample(address_list, sample_size)
+    return random_list
+
+
+def sort_key(list_item):
+    # Sort key specific to my data.
+    parts = list_item.split()
+    if len(parts) > 3:
+        return parts[1], int(parts[0]), parts[-1]
+    return parts[1], int(parts[0])
+
+
+def sort_addresses(address_list):
+    # Sort the data after randomization.
+    address_list.sort(key=sort_key)
+    return address_list
+
+
 def main():
-    pass
+   pass
 
 if __name__ == '__main__':
     main()
--- tests.py
+++ tests.py
@@ -1,5 +1,77 @@
-def main():
+from pathlib import Path
+import os
+import random_addresses as RA
+
+
+def setup_module(module):
     pass
 
-if __name__ == '__main__':
-    main()
+
+def teardown_module(module):
+    os.remove('test_write.txt')
+
+
+def test_file_open():
+    # Open file read to list.
+    address_list = RA.open_file('test_data.txt')
+    # Does the file open
+    assert address_list is not None
+    # Is the file a list
+    assert type(address_list) is list
+    # Are the list items striped
+    assert address_list[1] == '1001 N Summer LN UNIT E'
+    # list length is expected after empty lines are removed
+    assert len(address_list) == 24
+
+
+def test_write_file():
+    test_list = ['red', 'blue', 'green']
+    RA.write_file(test_list, 'test_write.txt')
+    my_file = Path('test_write.txt')
+    assert my_file.is_file() is True
+
+
+def test_determine_sample_size():
+    # Open file read to list.
+    address_list = RA.open_file('test_data.txt')
+    # sample half the list test_data is length 24.
+    sample_rate = 0.5
+    results = RA.determine_sample_size(address_list, sample_rate)
+    # half the list is 12
+    assert results == 12
+
+
+def test_random_sample():
+    # Open file read to list.
+    address_list = RA.open_file('test_data.txt')
+    # create random list
+    random_addresses = RA.random_sample(address_list, sample_size=12)
+    # make sure list length is as expected.
+    assert len(random_addresses) == 12
+
+
+def test_sort_addresses():
+    unsorted = ['515 Movie RD UNIT 204',
+                '2517 Blue LN',
+                '512 Movie RD UNIT 102',
+                '2506 Blue LN',
+                '507 Movie RD UNIT 204',
+                '1003 N Summer LN UNIT E',
+                '2702 Silver GATE WAY',
+                '1003 N Summer LN UNIT H',
+                '2610 Silver GATE WAY',
+                '1003 N Summer LN UNIT A']
+
+    expected = ['2506 Blue LN',
+                '2517 Blue LN',
+                '507 Movie RD UNIT 204',
+                '512 Movie RD UNIT 102',
+                '515 Movie RD UNIT 204',
+                '1003 N Summer LN UNIT A',
+                '1003 N Summer LN UNIT E',
+                '1003 N Summer LN UNIT H',
+                '2610 Silver GATE WAY',
+                '2702 Silver GATE WAY']
+
+    custom_sorted = RA.sort_addresses(unsorted)
+    assert custom_sorted == expected
