a3c02b5623c8827777ff72ee8fadd33bc3ac8a3b
Scott <scott48074@users.noreply.github.com>
2017-03-12 09:52:42 -0500

    Changed project structure added a try/catch

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='random_address',
     version='1.0',
-    packages=[],
+    packages=['scripts'],
     license='Unlicense',
     long_description=open('README.txt').read(),
 )
